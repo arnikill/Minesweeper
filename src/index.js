b2d132b6819a694573989dc39a955d0796e0cce7
aliya <tusupkalievaaliya921@gmail.com>
2023-10-02 15:17:03 +0500

    feat: added functions to display elapsed time and create a playing field

--- src/index.js
+++ src/index.js
@@ -44,16 +44,69 @@ document.addEventListener('DOMContentLoaded', () => {
     restartGame.classList.add('restartGame');
     restartGame.textContent = 'Restart Game';
 
-//параметры игры
-let startTime;
-let elapsedTime = 0;
-let clickCounter = 0;
-let timerInterval;
-let width = 10;
-let bombAmount = 20;
-let flags = 0;
-let cells = [];
-let isGameOver = false;
+    //параметры игры
+    let startTime;
+    let elapsedTime = 0;
+    let clickCounter = 0;
+    let timerInterval;
+    let width = 10;
+    let bombAmount = 20;
+    let flags = 0;
+    let cells = [];//тут храним все клетки
+    let isGameOver = false;
+
+    function updateTime() {// функция обновляет отображение прошедшего времени.
+        const currentTime = new Date().getTime()// текущее время в миллисекундах
+        elapsedTime = Math.floor((currentTime - startTime / 1000))// Затем она вычисляет разницу между текущим временем и startTime
+        timeElapsed.textContent = `Time: ${timeElapsed} seconds`//значение обновляется
+    }
+
+    function createBoard() {
+        startTime = new Date().getTime()
+        flagsLeft.innerHTML = bombAmount
+
+
+        const bombsArray = Array(bombAmount).fill('bomb')//кол-во опасных клеток
+        const emptyArray = Array(width * width - bombAmount).fill('actual')//кол-во безопасных клеток
+        const gameArray = emptyArray.concat(bombsArray)//объеденяем bombsArray empttyArray
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)//содержит случайное распределение бомб и безопасных клеток
+
+        for (let i = 0; i < width * width; i++) {
+            const cell = document.createElement('div')//создаю клетку
+            cell.setAttribute('id', i)//уникальный номер, который меняется каждый раз, когда  создаю новую клетку
+            cell.classList.add(shuffledArray[i])//Добавляет этой клетке определенный класс, чтобы модно было определить, является ли она бомбой или безопасной клеткой
+            board.appendChild(cell)//добавляет клетку в игровое поле
+            cells.push(cell)//добавляет эту клетку в специальный список клеток, чтобы можно было легко управлять ими в будущем
+
+            cell.addEventListener('click', function (e) {
+                //Когда пользователь кликает левой кнопкой мыши вызывается функция click, которая обрабатывает этот клик
+                click(cell)
+            })
+            cell.oncontextmenu = function (e) {
+                //это связано с кликами правой кнопкой мыши
+                e.preventDefault()//предотвращает стандартное контекстное меню браузера
+                addFlag(cell)//вызывается функция добавления флажка
+            }
+        }
+        //код помогает подсчитать и записать информацию о бомбах
+        for (let i = 0; i < cells.length; i++) {
+            let total = 0// используется для подсчета количества бомб вокруг текущей клетки.
+            let leftEdge = i % width === 0//определяет, находится ли текущая клетка на левом краю игравого поля
+            let rightEdge = i % width === width - 1//тут проверяет находится ли на правом
+            if (cells.classList.contains('actual')) {
+                if (i > 0 && !leftEdge && cells[i - 1].classList.contains('bomb')) total++
+                if (i > 9 && !rightEdge && cells[i + 1 - width].classList.contains('bomb')) total++
+                if (i > 10 && cells[i - width].classList.contains('bomb')) total++
+                if (i > 11 && !leftEdge && cells[i - 1 - width].classList.contains('bomb')) total++
+                if (i < 98 && !leftEdge && cells[i + 1].classList.contains('bomb')) total++
+                if (i < 90 && !leftEdge && cells[i - 1 + width].classList.contains('bomb')) total++
+                if (i < 88 && !leftEdge && cells[i + 1 + width].classList.contains('bomb')) total++
+                if (i < 89 && cells[i + width].classList.contains('bomb')) total++
+            }
+            cells.setAttribute('data', total)//при клике на клетку игрок может видеть сколько бомб рядом
+        }
+    }
+    createBoard()
 
 
 
